a72acee68c6afc2b95bea53a94e2e5c1d072a5f2
Eli Berger & Kenn Sugiyama <pair+erberger22.kennSugiyama@devbootcamp.com>
2017-04-27 16:16:01 -0700

    Add some partial view files for user auth

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  # Remember to create a migration!
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.password == password
+      @user
+    else
+      nil
+    end
+  end
+
+
+end
