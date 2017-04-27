dcccd821a156fcff10bf35e0724a2075fc3d506a
denise duffy <denise_duffy@denises-MacBook-Pro.local>
2017-04-27 16:06:51 -0700

    Add comments, answers, questions, and votes tables

---
+++ db/migrate/20170427153811_create_comments.rb
@@ -0,0 +1,8 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :comment_text, null: false
+      t.integer :commentable_id, null: false
+      t.integer :author_id, null: false
+
+      t.timestamps(null: false)
