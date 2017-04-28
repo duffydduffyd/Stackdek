6f5ffa2485685cf70ff7e3d265fb7710d219cc72
Eli Berger <email": null>
2017-04-28 12:02:22 -0700

    saving work before bug fix. List comments on question/show page

---
+++ app/controllers/answers.rb
@@ -0,0 +1,6 @@
+get '/answers/:id' do
+  @answer = Answer.find(params[:id])
+  @comments_on_answer = Comment.where(commentable_id: params[:id], commentable_type: 'answer')
+  erb :'answers/show'
+end
+
--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -8,5 +8,7 @@ end
 #GET QUESTION BY ID
 get '/questions/:id' do
   @question = Question.find(params[:id])
+  @answers = Answer.where(question_id: params[:id])
+  @comments_on_question = Comment.where(commentable_id: params[:id], commentable_type: 'question')
   erb :'/questions/show'
 end
