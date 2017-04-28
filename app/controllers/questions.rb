e79727b57dc5df0ff3c8153756b0abea38b7ca4b
denise duffy <denise_duffy@denises-MacBook-Pro.local>
2017-04-28 11:26:00 -0700

    Update sessions controller with login show logic, add question button and route

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -4,6 +4,20 @@ get '/questions' do
   erb :'/questions/index'
 end
 
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+#POST ROUTE FOR THE FORM
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect '/questions'
+  else
+    @errors = @question.errors.full_messages
+    erb :'questions/index'
+  end
+end
 
 #GET QUESTION BY ID
 get '/questions/:id' do
