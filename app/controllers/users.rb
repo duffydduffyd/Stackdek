a92df39ea3c7ff3c395a940107f92f2d57ece036
erberger22 <erberger22@gmail.com>
2017-04-30 19:52:47 -0700

    Fix some bugs when trying to view pages while not logged in

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -14,7 +14,7 @@ post '/users' do
   @user = User.new(params[:user])
   if @user.save
     session[:id] = @user.id
-    redirect "/users/#{@user.id}"
+    redirect "/"
   else
     @errors = @user.errors.full_messages
     erb :'users/new'
@@ -23,6 +23,7 @@ end
 
 get '/users/:id' do
   @user = User.find(params[:id])
+  @questions = @user.questions
   erb :'users/show'
 end
 
